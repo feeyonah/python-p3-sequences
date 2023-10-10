61d1dca59ea8f545c2006b8cc6f8cf3caa0201d1
fiona <omburafiona@gmail.com>
2023-10-10 12:05:22 +0300

    sequences done

--- lib/sequences.py
+++ lib/sequences.py
@@ -4,14 +4,14 @@ def print_fibonacci(length):
     if length < 0:
         return []
 
-    # Initialize the first two Fibonacci numbers
+    
     fibonacci_sequence = []
     if length > 0:
         fibonacci_sequence.append(0)
     if length > 1:
         fibonacci_sequence.append(1)
 
-    # Generate the Fibonacci sequence up to the specified length
+
     while len(fibonacci_sequence) < length:
         next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
         fibonacci_sequence.append(next_number)
